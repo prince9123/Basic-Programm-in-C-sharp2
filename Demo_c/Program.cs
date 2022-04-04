d4fc62fa2cb128f965b021c39f6bd3733634a995
prince <princekumar9123446828@gmail.com>
2022-04-04 17:45:35 +0530

    initial commit

---
+++ Demo_c/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Demo_c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //use of foreach loop
+            Char[] myArray = { 'h', 'e', 'l', 'l', 'o' };
+            foreach(Char ch in myArray)
+            {
+                Console.WriteLine(ch);
+            }
+
+
+        }
+    }
+}
